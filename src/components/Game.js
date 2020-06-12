08fa490d1c59fea57ac156efbde3c18e31b17b73
Ernie Hsiung <ernie@wyncode.co>
2020-06-12 14:55:47 -0400

    add checkWinner

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,9 +8,34 @@ const Game = () => {
     setIxNext(!isXNext);
   };
 
+  const calculateWinner = (squares) => {
+    const possibleLines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    // go over all possibly winning lines and check if they consist of only X's/only O's
+    for (let i = 0; i < possibleLines.length; i++) {
+      const [a, b, c] = possibleLines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
   const getStatus = () => {
-    if (false) {
-      return "Winner: " + false;
+    if (calculateWinner(squares)) {
+      return "Winner: " + calculateWinner(squares);
     } else if (isBoardFull(squares)) {
       return "Draw!";
     } else {
@@ -29,7 +54,7 @@ const Game = () => {
   };
 
   const handleClick = (squareIndex) => {
-    if (squares[squareIndex] !== null) {
+    if (squares[squareIndex] !== null || calculateWinner(squares) !== null) {
       return;
     }
     console.log(squareIndex, squares);
