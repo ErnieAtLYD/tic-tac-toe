1176590fc6d7922563cfa79ece7268ad6e3040d3
Ernie Hsiung <ernie@wyncode.co>
2020-06-12 13:21:38 -0400

    add onClick to squares, refactor shortly

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,15 +2,41 @@ import React, { useState } from "react";
 const Game = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [isXNext, setIxNext] = useState(true);
+
+  const handleClick = (squareIndex) => {
+    console.log(squareIndex, squares);
+  };
+
   return (
     <>
       <div className="container">
         <div className="game">
           <div className="game-board">
             <div className="board-row">
-              <div className="square">{squares[0]}</div>
-              <div className="square">{squares[1]}</div>
-              <div className="square">{squares[2]}</div>
+              <div
+                onClick={() => {
+                  handleClick(0);
+                }}
+                className="square"
+              >
+                {squares[0]}
+              </div>
+              <div
+                onClick={() => {
+                  handleClick(1);
+                }}
+                className="square"
+              >
+                {squares[1]}
+              </div>
+              <div
+                onClick={() => {
+                  handleClick(2);
+                }}
+                className="square"
+              >
+                {squares[2]}
+              </div>
             </div>
             <div className="board-row">
               <div className="square">{squares[3]}</div>
